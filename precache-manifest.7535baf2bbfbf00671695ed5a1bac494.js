feaed8bfc07abd6d015bd07e456adbc1455b7cd3
dohaicuong <beatyshot@gmail.com>
2020-05-12 04:45:15 +0000

    Deploying to master from  @ 47efee7fdfc5f9bed6a3bf8bf59b515bf9505f26 🚀

---
+++ precache-manifest.7535baf2bbfbf00671695ed5a1bac494.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "110966356d52ce16881b67562a18bec8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1feb302f63d3bdf233ae",
+    "url": "/static/js/2.df636a9f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.df636a9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d5b1871253a27b9d5fc",
+    "url": "/static/js/3.9a61a203.chunk.js"
+  },
+  {
+    "revision": "3343e001ca7ba96bc06c",
+    "url": "/static/js/4.ac1bc931.chunk.js"
+  },
+  {
+    "revision": "08a2e5707f0841d6f583",
+    "url": "/static/js/5.7c572546.chunk.js"
+  },
+  {
+    "revision": "b9978772195b12857faa",
+    "url": "/static/js/6.a9893c12.chunk.js"
+  },
+  {
+    "revision": "7e0bf3def3537998f8ba",
+    "url": "/static/js/7.537ac7d2.chunk.js"
+  },
+  {
+    "revision": "0b2f57cfbb6d4abb757a",
+    "url": "/static/js/8.f7813f7f.chunk.js"
+  },
+  {
+    "revision": "bda64998e3abb976ec10",
+    "url": "/static/js/main.281914ba.chunk.js"
+  },
+  {
+    "revision": "89f00de4d83a5f7af6fb",
+    "url": "/static/js/runtime-main.c745a9b0.js"
+  },
+  {
+    "revision": "3aaaa55cb40578802817dfb850459360",
+    "url": "/static/media/about.3aaaa55c.svg"
+  },
+  {
+    "revision": "64fb8a6c031e021a72de8ea970588908",
+    "url": "/static/media/contact.64fb8a6c.svg"
+  }
+]);
